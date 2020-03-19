95599d8c847d94663a25fce31f0d5b546367470e
Vinay S <vinay-s@hcl.com>
2020-03-19 17:16:17 +0530

    Additional changes

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,17 +6,35 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+    servermessage = 'No server available!';
     serverID:number=10;
     serverStatus:string = 'Online';
+    serverName='Linux';
+    serverCondition = false;
 
     getServerStatus(){
       return this.serverStatus;
     }
 
+
+    getColor(){
+        return this.serverStatus === 'Online' ? 'green' : 'red';
+    }
     serverState:boolean = true;
     constructor(){
-        setTimeout(() => {
-            this.serverState=false;
-        },2000);
+        // setTimeout(() => {
+        //     this.serverState=false;
+        // },2000);
+
+       this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+    }
+
+    onServer(){
+        this.serverCondition=true;
+       this.servermessage='Server is available and server name is'+this.serverName;
+    }
+
+    onServerName(event:Event){
+        this.serverName = (<HTMLInputElement>event.target).value;
     }
 }
